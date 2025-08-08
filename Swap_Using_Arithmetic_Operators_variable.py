83e522959f7a42366d37513778474ee579849d5f
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-08-08 17:15:25 +0530

    swap two variable using Arithmetic Operator

---
+++ Swap_Using_Arithmetic_Operators_variable.py
@@ -0,0 +1,20 @@
+#Python program to swap two variables 
+# Using Arithmrtic Operators variable. 
+
+#Using + - operators
+a=5
+b=4
+print(f"a is {a} b is {b}")
+a=a+b
+b=a-b
+a=a-b
+print(f"a is {a} b is {b}")
+
+#Using * / operators
+a=7
+b=8
+print(f"a is {a} b is {b}")
+a=a*b
+b=a/b
+a=a/b
+print(f"a is {a} b is {b}")
