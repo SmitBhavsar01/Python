5b726de8bce69364fc3e9427a22f2fba28af9f30
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-08-09 10:24:27 +0530

    sum of n natural numbers without loop

---
+++ find_sum_n_natural_numbers_without_loop.py
@@ -0,0 +1,8 @@
+# Python program to find sum of 
+# n natural numbers without loop.
+
+n=int(input("Enter the n Natural numbers you want to sum: ")
+)
+sum = (n*(n + 1))//2
+
+print(f"sum is : {sum}")
