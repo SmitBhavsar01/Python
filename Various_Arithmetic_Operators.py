e655851918c850dab01241121a6731de61d8a2dd
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-08-09 10:32:34 +0530

    various arithmetic operators

---
+++ Various_Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+#Python Program to Check  
+#various arithmetic operators.
+a=int(input("Enter the variable a :"))
+b=int(input("Enter the variable b :"))
+
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication :", a*b)
+print("Division :", a/b)
+print("Exponentiation :", a**b)
+print ("Modulus :", a%b)
+
+
